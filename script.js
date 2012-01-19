671cad5e74f2cfc26fd13bd0efa6d55e90c193f4
Aleksandr Gornostal <info@sanya.pp.ua>
2012-01-19 23:04:08 +0200

    redesigned

---
+++ script.js
@@ -0,0 +1,159 @@
+;(function($){
+    
+    $(function(){
+        var $bm = $('#bookmarks');
+        $bm.CromeBookmarks('Bookmarks', {
+            persist: true,
+            loadFavicons: false
+        });
+    });
+})(jQuery);
+
+/** 
+ * CromeBookmarks plugin 
+ */
+(function($){
+    $.fn.CromeBookmarks = function (url, options) {
+        var that = this;
+        var defaults = {
+            persist: true,
+            loadFavicons: false,
+            cookieid: 'chrome-bookmarks'
+        };
+        options = $.extend({}, defaults, options);
+
+        var getBookmarks = function(data){
+            var ret = '';
+
+            for (var i in data) {
+                if ('type' in data[i] && 'name' in data[i] && data[i].name != 'Synced bookmarks') {
+
+                    var type      = data[i].type == 'folder' ? 'folder' : 'file',
+                        className = type == 'folder' ? 'class="folder"' : '',
+                        url       = type == 'folder' ? 'javascript:void(0);' : data[i].url,
+                        a         = '<a id="item-' + data[i].id + '" data-item="' + data[i].id + '" href="' + url + '">' + data[i].name + '</a>',
+                        li        = '<li ' + className + '>' + a;
+
+                    if (type == 'folder' && data[i].children.length) {
+                        li += '<ul>' + getBookmarks(data[i].children) + '</ul>';
+                    }
+                    li += '</li>';
+
+                    ret += li;
+                }
+            }
+
+            return ret;
+        };
+        
+        var opened = {};
+        var getOpened = function(){
+            var cookies = $.cookie(options.cookieid);
+            var ret = {};
+            if (cookies) {
+                var arr = cookies.split(',');
+                for (var i = 0; i < arr.length; i++) {
+                    var id = 'item-'+arr[i];
+                    var parent = $('#'+id).parent();
+                    if (parent.length) {
+                        parent.addClass('open');
+                        ret[id] = arr[i];
+                    }
+                }
+            }
+            return ret;
+        };
+        var saveOpened = function(){
+            var arr = [];
+            for (var i in opened) {
+                arr.push(opened[i]);
+            }
+            $.cookie(options.cookieid, arr.join(','));
+        }
+        var toggleFolder = function(){
+            var id = $(this).data('item');
+            var li = $(this).parent();
+            var open = li.hasClass('open') ? true : false;
+            if (open) {
+                li.removeClass('open');
+                if ('item-'+id in opened) {
+                    delete opened['item-'+id];
+                }
+            } else {
+                li.addClass('open');
+                opened['item-'+id] = id;
+            }
+            saveOpened();
+            return false;
+        }
+
+        var cacheBuster = Math.round(new Date()/(1000*60));
+        $.ajax({
+            url: url,
+            data: {cb: cacheBuster},
+            dataType: 'json',
+            success: function(data){
+                $(that).html(getBookmarks(data.roots));
+                $('.folder > a', that).click(toggleFolder);
+                opened = getOpened();
+                $(that).css({display: 'block'});
+            },
+            error: function(resp) {
+                if (resp.status == 404) {
+                    $(that).text("File '" + url + "' not found!").css('color', 'red');
+                }
+            }
+        });
+    }
+})(jQuery);
+
+
+/**
+ * jQuery Cookie plugin
+ *
+ * Copyright (c) 2010 Klaus Hartl (stilbuero.de)
+ * Dual licensed under the MIT and GPL licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ */
+(function($) {
+    $.cookie = function(key, value, options) {
+
+        // key and at least value given, set cookie...
+        if (arguments.length > 1 && (Object.prototype.toString.call(value) === "[object String]" || value === null || value === undefined)) {
+            options = $.extend({}, options);
+
+            if (value === null || value === undefined) {
+                options.expires = -1;
+            }
+
+            if (typeof options.expires === 'number') {
+                var days = options.expires, t = options.expires = new Date();
+                t.setDate(t.getDate() + days);
+            }
+
+            value = String(value);
+
+            return (document.cookie = [
+                encodeURIComponent(key), '=',
+                options.raw ? value : encodeURIComponent(value),
+                options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
+                options.path ? '; path=' + options.path : '',
+                options.domain ? '; domain=' + options.domain : '',
+                options.secure ? '; secure' : ''
+            ].join(''));
+        }
+
+        // key and possibly options given, get cookie...
+        options = value || {};
+        var decode = options.raw ? function(s) {return s;} : decodeURIComponent;
+
+        var parts = document.cookie.split('; ');
+        for (var i = 0, part; part = parts[i]; i++) {
+            var pair = part.split('=');
+            if (decode(pair[0]) === key) return decode(pair[1] || ''); // IE saves cookies with empty string as "c; ", e.g. without "=" as opposed to EOMB
+        }
+        return null;
+    };
+})(jQuery);
